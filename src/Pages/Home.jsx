c36e61bf644650408b96b523725da75055c64149
Hanss <hanss.rivera@gmail.com>
2024-12-25 18:57:39 -0700

    pre-css overhaul

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,3 @@
-import HomeS from "./Home.module.css"
 import NavBar from "../Components/NavBar.jsx"
 
 function Home(){
@@ -6,28 +5,7 @@ function Home(){
         <>
         <NavBar/>
         <div className="container">
-            <table className={HomeS}>
-                <thead>
-                    <tr >
-                        <th scope="col">#</th>
-                        <th scope="col">Image</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Score</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Notes</th>
-                    </tr>
-                    <tr>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>a</td>
-                        <td>a</td>
-                    </tr>
-                    
-                </thead>
-                <tbody></tbody>
-            </table>
+        
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad eum quibusdam vitae animi eveniet rem incidunt porro odit! Iste assumenda quo esse labore. Maiores quam assumenda reprehenderit quaerat tenetur.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad eum quibusdam vitae animi eveniet rem incidunt porro odit! Iste assumenda quo esse labore. Maiores quam assumenda reprehenderit quaerat tenetur.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ad eum quibusdam vitae animi eveniet rem incidunt porro odit! Iste assumenda quo esse labore. Maiores quam assumenda reprehenderit quaerat tenetur.
--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -7,7 +7,7 @@ function SearchPage(){
     return(
         <>
         <NavBar/>
-        <div className="container">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis quos culpa possimus similique expedita! Pariatur rem exercitationem natus aspernatur voluptatum quidem tempore cum, magnam facilis deleniti, laboriosam ipsum dicta corrupti.</div>
+        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis quos culpa possimus similique expedita! Pariatur rem exercitationem natus aspernatur voluptatum quidem tempore cum, magnam facilis deleniti, laboriosam ipsum dicta corrupti.</div>
         
   
         
